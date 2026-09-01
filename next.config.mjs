4f401aa783e5dd1f023ce7f05c8a9db36975b301
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-01 04:57:27 +0300

    config: update tsconfig settings and set turbopack root

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
+  experimental: {
+    turbopack: {
+      root: 'C:\\Users\\Selam Yibeltal\\smart-login-page',
+    },
   },
 };
 
